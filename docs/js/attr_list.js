cf982345456e5dabf858efe4401a99ede689060c
Erik Båvenstrand <erik.bavenstrands@gmail.com>
2020-03-02 13:17:02 +0100

    Attr list is now entirely in d3

--- docs/js/attr_list.js
+++ docs/js/attr_list.js
@@ -1,87 +1,53 @@
 /*! viral-50 v0.0.1 | (c) 2020 Erik Båvenstrand | MIT License | https://github.com/ErikBavenstrand/DH2321-Spotify-Project */
-function setAttrBtn(attr) {
-  btn = document.getElementById("attr-btn-label");
-  btn.innerHTML = attr;
-}
-
-/*function listAttributes(attrs) {
-  ul = document.getElementById("attr-list-items");
-
-  attrs.forEach(function(element) {
-    var li = document.createElement("li");
-    li.setAttribute("id", "attr-list-item-" + element);
-    li.setAttribute("text", element);
-    li.setAttribute("class", "mdl-menu__item");
-    li.innerHTML = element;
-
-    //Update map functionality
-    li.onclick = function() {
-      currentAttribute = element;
-      btn = document.getElementById("demo-menu-lower-left");
-      btn.innerHTML = element;
-      updateWorldMap(data[dataWeek], data.minimum, data.maximum);
-    };
-
-    ul.appendChild(li);
-  });
-}*/
-
-function listAttributes(attrs) {
-  ul = document.getElementById("dropdown-container");
-  var firstEle = true;
-  attrs.forEach((function(element) {
-    var li = document.createElement("li");
-    li.innerHTML = element;
-
-    if (firstEle) {
-      li.setAttribute("style", "border: none;");
-      firstEle = false;
-    }
-
-    li.onclick = function() {
-      currentAttribute = element;
-      btnLabel = document.getElementById("attr-btn-label");
-      btnLabel.innerHTML = element;
+function listAttributes(attributes) {
+  var ul = d3.select("#dropdown-container");
+  var buttonLabel = d3.select("#attr-btn-label");
+  ul.selectAll("li")
+    .data(attributes)
+    .enter()
+    .append("li")
+    .text((function(d) {
+      return d;
+    }))
+    .on("click", (function(d) {
+      currentAttribute = d;
+      buttonLabel.text((function() {
+        return d;
+      }));
       updateWorldMap(data[dataWeek], data.minimum, data.maximum);
       toggleDropdown();
-    };
-
-    ul.appendChild(li);
-  }));
+    }));
 }
 
 function loadAttrList() {
-  var key1, key2;
-  // Get first item so that we can get attribute list.
-  for (var i in data) {
-    key1 = i;
-    for (var j in data[i]) {
-      key2 = j;
-      break;
-    }
-    break;
-  }
+  var buttonLabel = d3.select("#attr-btn-label");
+  var firstDate = Object.keys(data)[0];
+  var firstCountry = Object.keys(data[firstDate])[0];
+  var attributes = Object.keys(data[firstDate][firstCountry]);
+  listAttributes(attributes);
+  buttonLabel.text((function() {
+    return currentAttribute;
+  }));
 
-  attrs = Object.keys(data[key1][key2]);
-  listAttributes(attrs);
-  setAttrBtn(currentAttribute);
+  var dropdownBtn = d3.select("#attr-button");
+  dropdownBtn.on("click", (function() {
+    toggleDropdown();
+  }));
 }
 
 function toggleDropdown() {
-  var dropdownEleCont = document.getElementById("dropdown-container");
-  var dropdownArrow = document.getElementById("drop-img");
+  var dropdownElementContainer = d3.select("#dropdown-container");
+  var dropdownArrow = d3.select("#drop-img");
 
-  if (dropdownEleCont.className == "") {
-    dropdownEleCont.className = "toggleDropdown";
-    dropdownArrow.innerHTML = "arrow_drop_up";
+  if (dropdownElementContainer.classed("toggleDropdown")) {
+    dropdownElementContainer.classed("toggleDropdown", false);
+    dropdownArrow.text((function() {
+      return "arrow_drop_down";
+    }));
   } else {
-    dropdownEleCont.className = "";
-    dropdownArrow.innerHTML = "arrow_drop_down";
+    dropdownElementContainer.classed("toggleDropdown", true);
+    dropdownArrow.text((function() {
+      return "arrow_drop_up";
+    }));
   }
 }
-
-var dropdownBtn = document.getElementById("attr-button");
-
-dropdownBtn.onclick = function() {
-  toggleDropdown();
-};
