b18d2dd70658a6f86e7773ac415b2b79859a45c6
Martin Falke <mafalke@kth.se>
2020-03-09 12:41:12 +0100

    Styling changes made, primarily to the selected week at bottom left but also to the background color at bottom

--- docs/js/timeslider.js
+++ docs/js/timeslider.js
@@ -67,7 +67,7 @@ function loadTimeSlider() {
   sliderInput.on("input", (function() {
     updateTimeSliderStyle(155 - this.value);
     dataWeek = valueToWeek(155 - this.value);
-    d3.select("#week-label").text("Week: " + dataWeek);
+    d3.select("#week-label").text("WEEK OF " + dataWeek);
     updateWorldMap(
       data_attrs[dataWeek],
       data_attrs.minimum,
