96e5edaee91945fc514dc6f521aca22e0a1aae02
Martin Falke <mafalke@kth.se>
2020-03-10 18:19:12 +0100

    Loading screen completed with removal on files loaded

--- src/js/main.js
+++ src/js/main.js
@@ -27,6 +27,12 @@ Promise.all([
   d3.json("data/countries.json"),
   d3.json("data/week_countries_songs_normalized.json")
 ]).then(function(files) {
+  // fade out and then remove the loading screen wrapper node
+  d3.select(".Loading__wrapper").transition()
+    .style("opacity", 0)
+    .duration(800)
+    .on("end", function(){ d3.select(this).remove(); });
+  // save the loaded files and initialize the app
   filesLoaded = true;
   data_attrs = files[0];
   worldJSON = files[1];
